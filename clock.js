1d59972770c71e8820d3816ece2ac1662e2931d0
sungwookang <sungwookang11@gmail.com>
2022-01-02 19:54:47 +0900

    complete clock

--- clock.js
+++ clock.js
@@ -1,18 +1,17 @@
-const clockContainer = document.querySelector('.js-clock'),
-  clockTitle = clockContainer.querySelector('h1');
+const clockContainer = document.querySelector(".clock");
+const clockTitle = clockContainer.querySelector("h1");
+ 
+getTime();
 
 function getTime() {
   const date = new Date();
-  const seconds = date.getSeconds();
-  const minutes = date.getMinutes();
   const hours = date.getHours();
-	clockTitle.innerText = `${"hours":"minutes":"seconds"}`;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
-
+ 
+  
 setInterval(getTime, 1000);
-
-function init() {
-  getTime();
-}
-
-init();
--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const loginInForm = document.querySelector(".login-form");
-const loginInput = document.querySelector(".login-form input");
+const clockContainer = document.querySelector(".clock");
+const clockTitle = clockContainer.querySelector("h1");
+ 
+getTime();
 
-function loginSubmit () {
-    const username = loginInput.value;
-    console.log(username);
-} 
-
-loginInForm.addEventListener("submit", loginSubmit);
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+ 
+  
+setInterval(getTime, 1000);
