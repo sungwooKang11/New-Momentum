3881d2fdf4974a6abe01b843ae28fa34d5e53443
sungwookang <sungwookang11@gmail.com>
2022-01-03 01:54:16 +0900

    complete local login

--- clock.js
+++ clock.js
@@ -8,10 +8,9 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:
+              ${minutes < 10 ? `0${minutes}` : minutes}:
+              ${seconds < 10 ? `0${seconds}` : seconds}`;
 }
  
-  
-setInterval(getTime, 1000);
+  setInterval(getTime, 1000);
--- script.js
+++ script.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const hello = document.querySelector("#hello");
 
 const H = "hidden";
+const UN = "username";
+
+//function fillHello(username) {
+    //hello.classList.remove(H);
+    //hello.innerText = "Hello " + username;}
 
 function loginSubmit (event) {
     event.preventDefault(); //유저가 이름 제출할 때, 기본동작 제어
     loginForm.classList.add(H); //처음 폼에 클래스 지정
     const username = loginInput.value; //유저 이름 저장
-    hello.innerText = "Hello " + username; 
+    localStorage.setItem(UN, username); //로컬스토리지에 유저이름 저장
+    hello.innerText = "Hello " + username; // `Hello ${username}`; 도 동일한 표기임
     hello.classList.remove(H);
 } 
 
-loginForm.addEventListener("submit", loginSubmit);
+const lsUsername = localStorage.getItem(UN);
 
+if(lsUsername === null){
+    loginForm.classList.remove(H);
+    loginForm.addEventListener("submit", loginSubmit);
+}else {
+    hello.classList.remove(H);
+    hello.innerText = "Hello " + lsUsername;
+}
