9310f62740b8b1ca08ba595688a192ff70a86954
sungwookang <sungwookang11@gmail.com>
2022-01-03 03:38:37 +0900

    add background

--- qoutes.js
+++ qoutes.js
@@ -44,7 +44,7 @@ const quotes = [
   
   const quote = document.querySelector("#quote span:first-child");
   const author = document.querySelector("#quote span:last-child");
-  const todaysQuote = quotes[Math.floor(Math.random() * 10)];
+  const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
   
   quote.innerText = todaysQuote.quot;
   author.innerText = todaysQuote.author;
