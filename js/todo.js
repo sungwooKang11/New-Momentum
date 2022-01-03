58f9535ed81ac55a3909663e0924ad1f40fa7493
sungwookang <sungwookang11@gmail.com>
2022-01-03 23:11:37 +0900

    adding todo lists

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+function fillToDo(newToDo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    todoList.appendChild(li);
+}
+
 function handleToDo(event) {
     event.preventDefault();
     const newToDo = todoInput.value; 
     todoInput.value = "";
+    fillToDo(newToDo);
 }
 
 todoForm.addEventListener("submit", handleToDo);
